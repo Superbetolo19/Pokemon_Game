74ad891344381aecc2d9c39f5cc721518632740f
Superbetolo19 <99560445+Superbetolo19@users.noreply.github.com>
2024-04-25 13:41:19 -0600

    Add files via upload

---
+++ Pokémon/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Pokémon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(204, 266);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(135, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label4.Location = new System.Drawing.Point(560, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.Location = new System.Drawing.Point(377, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "TIME LEFT:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(119, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ":";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(122, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = global::Pokémon.Properties.Resources.Fondopokemon;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(636, 327);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Image = global::Pokémon.Properties.Resources.ScoreLabel;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Score:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 327);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pokémon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Pokémon/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Media;
+using System.Threading;
+
+namespace Pokémon
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player;
+        int timeleft = 40;
+        int currenttime = 0;
+        int score = 0;
+        int ok = -1;
+        String foto;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //pictureBox1.Image=Image.FromFile("")
+            cargar();
+            
+        }
+
+        public async void cargar()
+        {
+            if (timeleft > 0)
+            {
+                //Thread.Sleep(3000);
+                ReproducirSonido();
+                currenttime = 0;
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                var client = new HttpClient();
+                var url = "https://localhost/so/pokemon/index.php";
+                var res = await client.GetAsync(url);
+                var json = await res.Content.ReadAsStringAsync();
+                Root data = JsonConvert.DeserializeObject<Root>(json);
+                ok = data.ok;
+
+                /*for(int i=0; i< data.array.Count; i++)
+                {
+                    this.button1.Text(data.array.ElementAt(i).nom.ToString());
+                }*/
+                button1.Text = data.array.ElementAt(0).nom.ToString();
+                button2.Text = data.array.ElementAt(1).nom.ToString();
+                button3.Text = data.array.ElementAt(2).nom.ToString();
+                button4.Text = data.array.ElementAt(3).nom.ToString();
+                button5.Text = data.array.ElementAt(4).nom.ToString();
+                Stream StreamImage;
+                StreamImage = getUrl("https://localhost/so/pokemon/" + data.array.ElementAt(ok).black.ToString());
+                pictureBox1.Image = System.Drawing.Image.FromStream(StreamImage);
+                foto = "https://localhost/so/pokemon/" + data.array.ElementAt(ok).foto.ToString();
+                this.timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("GAME OVER\nTu puntuación fue de: " + score.ToString());
+            }
+            
+        }
+
+        private void CalcularScore(int tiempo)
+        {
+            if (tiempo < 10)
+            {
+                score += 100 * (10 - tiempo);
+            }
+            this.label2.Text=score.ToString();
+        }
+
+        private void ReproducirSonido()
+        {
+            player = new SoundPlayer(@"C:\Users\megab\source\repos\Pokémon\audio1.wav");
+            try
+            {
+                // Reproducimos el sonido
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al reproducir el sonido: " + ex.Message);
+            }
+        }
+
+        private Stream getUrl(string URL)
+        {
+
+            string strResp = "";
+            HttpWebRequest request = ((HttpWebRequest)WebRequest.Create(URL));
+
+            HttpWebResponse response = ((HttpWebResponse)request.GetResponse());
+
+            try
+            {
+
+                return response.GetResponseStream();
+
+            }
+            catch
+            {
+                return response.GetResponseStream();
+                //elError = ex.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (ok == 0)
+            {
+                timeleft += 3;
+                CalcularScore(currenttime);
+                MessageBox.Show("Es Correcto, deberías tocar pasto");
+                
+            } else
+            {
+                timeleft -= 5;
+                MessageBox.Show("Incorrecto, deberías reconsiderar que has hecho con tu vida");
+            }
+            Stream StreamImage;
+            StreamImage = getUrl(foto);
+            pictureBox1.Image = System.Drawing.Image.FromStream(StreamImage);
+            //Thread.Sleep(3000);
+            timer2.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (ok == 1)
+            {
+                CalcularScore(currenttime);
+                MessageBox.Show("Es Correcto, deberías tocar pasto");
+               
+            }
+            else
+            {
+                timeleft -= 5;
+                MessageBox.Show("Incorrecto, deberías reconsiderar que has hecho con tu vida");
+            }
+            Stream StreamImage;
+            StreamImage = getUrl(foto);
+            pictureBox1.Image = System.Drawing.Image.FromStream(StreamImage);
+            //Thread.Sleep(3000);
+            timer2.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (ok == 2)
+            {
+                CalcularScore(currenttime);
+                MessageBox.Show("Es Correcto, deberías tocar pasto");
+                
+            }
+            else
+            {
+                timeleft -= 5;
+                MessageBox.Show("Incorrecto, deberías reconsiderar que has hecho con tu vida");
+            }
+            Stream StreamImage;
+            StreamImage = getUrl(foto);
+            pictureBox1.Image = System.Drawing.Image.FromStream(StreamImage);
+            //Thread.Sleep(3000);
+            timer2.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (ok == 3)
+            {
+                CalcularScore(currenttime);
+                MessageBox.Show("Es Correcto, deberías tocar pasto");
+                
+            }
+            else
+            {
+                timeleft -= 5;
+                MessageBox.Show("Incorrecto, deberías reconsiderar que has hecho con tu vida");
+            }
+            Stream StreamImage;
+            StreamImage = getUrl(foto);
+            pictureBox1.Image = System.Drawing.Image.FromStream(StreamImage);
+            //Thread.Sleep(3000);
+            timer2.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (ok == 4)
+            {
+                CalcularScore(currenttime);
+                MessageBox.Show("Es Correcto, deberías tocar pasto");
+                
+            }
+            else
+            {
+                timeleft -= 5;
+                MessageBox.Show("Incorrecto, deberías reconsiderar que has hecho con tu vida");
+            }
+            Stream StreamImage;
+            StreamImage = getUrl(foto);
+            pictureBox1.Image = System.Drawing.Image.FromStream(StreamImage);
+            //Thread.Sleep(3000);
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeleft--;
+            currenttime++;
+            this.label4.Text = timeleft.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            cargar();
+            timer2.Stop();
+        }
+    }
+
+    public class Array
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+        public string link { get; set; }
+        public string foto { get; set; }
+        public string black { get; set; }
+    }
+
+    public class Root
+    {
+        public int ok { get; set; }
+        public List<Array> array { get; set; }
+    }
+
+}
